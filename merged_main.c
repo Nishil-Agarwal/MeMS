b5dc1c47d9cf595e854644c79e766287f3e7d1dd
Nishil-Agarwal <132306904+Nishil-Agarwal@users.noreply.github.com>
2023-11-03 21:57:07 +0530

    Debugged, not yet checked functioning

--- merged_main.c
+++ merged_main.c
@@ -7,9 +7,9 @@
 struct node {
     struct node* prev;
     struct node* next;
-    int mmaped_physical_address;         //Physical add where mmaped page(s) worth of memory is starting from
-    int mmaped_page_size;                //Total contiguous memory in bytes allocated from mmap for this main node
-    struct node* lower_linklist_reference_ptr;         //ptr to 1st node of lower linked list
+    void* mmaped_physical_address;         //Physical add where mmaped page(s) worth of memory is starting from
+    size_t mmaped_page_size;                //Total contiguous memory in bytes allocated from mmap for this main node
+    struct lownode* lower_linklist_reference_ptr;         //ptr to 1st node of lower linked list
     int virtual_add_starting_point_for_this_row;
 };
 
@@ -19,7 +19,7 @@ struct lownode {
     struct lownode* prev;
     int virtual_address;
     int status;       //1 = hole, 0 = occupied
-    int size;         //Node allocated size
+    size_t size;         //Node allocated size
 };
 
 struct node* header_list_space;
@@ -27,52 +27,16 @@ void* current_structure_page_ptr;
 void* current;
 struct node* previous_upper_list_node=NULL;
 
-int traversal_allocate_process(int sizerequired){
-    //complete this to find large enough hole. return 1 on success.
-    //additional task (if enough time left) : when large enough hole is found, edit lower link list to split hole into 2 parts -> now a occupied portion, leftover hole
-    //if no big enough hole then return -1
-    struct lownode* lowtraversal;
-    struct node* uppertraversal;
-    uppertraversal=header_list_space;
-    if (header_list_space!=NULL){
-        int flag=0;
-        do{ 
-            lowtraversal=uppertraversal->lower_linklist_reference_ptr;
-        // munmap(ptr, usersize);
-            do{ 
-                size_t struct_sizelower = sizeof(struct lownode*);
-                if (lowtraversal -> status == 1 && lowtraversal-> size > sizerequired ){
-                    size_t totsize = lowtraversal -> size ;
-                    void* curmemlocation = lowtraversal -> memory_allocated_ptr;
-                    int curvirtualaddress = lowtraversal -> virtual_address;
-                    struct lownode* temp = create_lowernode(curmemlocation+sizerequired, curvirtualaddress+sizerequired,1,totsize-sizerequired);
 
-                    struct lownode* nextelement = lowtraversal->next;
-                    lowtraversal -> next = temp;
-                    temp->next = nextelement;
-                    temp->prev = lowtraversal;
-                    nextelement->prev=temp;
-                    lowtraversal -> status = 0;
-                    lowtraversal->size = sizerequired;
-                }
-
-                else{
-                    lowtraversal = lowtraversal -> next;
-                    }
-            }while(lowtraversal->next!=NULL);
-            uppertraversal=uppertraversal->next;
-        }while(uppertraversal->next!=NULL);
-        if (flag==0){
-            return -1;
-        }else{
-            return 1;
-        }
-    }else{
-        return -1;
-    }
+void* allocatespace(int requested_size){
+    int pagesreqd = requested_size/PAGE_SIZE+1;
+    size_t reqdsize = pagesreqd*PAGE_SIZE;
+    void* heap_starts_at_this_physical_address=mmap(NULL, reqdsize, PROT_READ|PROT_WRITE, MAP_PRIVATE|MAP_ANONYMOUS, -1, 0);
+    return heap_starts_at_this_physical_address;
 }
 
-struct lownode* create_lowernode(void* mem_alloc_ptr,int vir_add, int stats,int sizereq){
+
+struct lownode* create_lowernode(void* mem_alloc_ptr,int vir_add, int stats,size_t sizereq){
     if (current+sizeof(struct lownode*)>current_structure_page_ptr+PAGE_SIZE){
         struct node* newpage=mmap(NULL, PAGE_SIZE, PROT_READ|PROT_WRITE, MAP_PRIVATE|MAP_ANONYMOUS, -1, 0);
         current=newpage;
@@ -89,7 +53,7 @@ struct lownode* create_lowernode(void* mem_alloc_ptr,int vir_add, int stats,int 
 }
 
 
-void insert_uppernode(int sizerequired){
+void* insert_uppernode(size_t sizerequired){
     void* alloted_address;
     int pages;
     if (current+sizeof(struct node*)>current_structure_page_ptr+PAGE_SIZE){
@@ -107,9 +71,10 @@ void insert_uppernode(int sizerequired){
         previous_upper_list_node->next=(struct node*)current;
         ((struct node*)current)->virtual_add_starting_point_for_this_row=(previous_upper_list_node->virtual_add_starting_point_for_this_row)+1;
     }
-    void* alloted_address=allocatespace(sizerequired);
+
+    alloted_address=allocatespace((sizerequired/PAGE_SIZE+1)*PAGE_SIZE);
     ((struct node*)current)->mmaped_physical_address=alloted_address;
-    int pages=(sizerequired/PAGE_SIZE+1);
+    pages=(sizerequired/PAGE_SIZE+1);
     ((struct node*)current)->mmaped_page_size=pages*PAGE_SIZE;
     if (previous_upper_list_node!=NULL){
         previous_upper_list_node=previous_upper_list_node->next;
@@ -148,19 +113,67 @@ void insert_uppernode(int sizerequired){
         ((struct lownode*)current)->virtual_address=((struct lownode*)prev_node_temp)->virtual_address+((struct lownode*)prev_node_temp)->size;
         current+sizeof(struct lownode*);
     }
+    return alloted_address;
 }
 
 
-void* allocatespace(int requested_size){
-    int pagesreqd = requested_size/PAGE_SIZE+1;
-    size_t reqdsize = pagesreqd*PAGE_SIZE;
-    void* heap_starts_at_this_physical_address=mmap(NULL, reqdsize, PROT_READ|PROT_WRITE, MAP_PRIVATE|MAP_ANONYMOUS, -1, 0);
-    return heap_starts_at_this_physical_address;
+int traversal_allocate_process(size_t sizerequired){
+    //complete this to find large enough hole. return 1 on success.
+    //if no big enough hole then return -1
+    struct lownode* lowtraversal;
+    struct node* uppertraversal;
+    uppertraversal=header_list_space;
+    if (header_list_space!=NULL){
+        int flag=0;
+        size_t struct_sizelower = sizeof(struct lownode*);
+        do{ 
+            lowtraversal=uppertraversal->lower_linklist_reference_ptr;
+        // munmap(ptr, usersize);
+            do{ 
+                if (lowtraversal -> status == 1 && lowtraversal-> size > sizerequired ){
+                    size_t totsize = lowtraversal -> size ;
+                    void* curmemlocation = lowtraversal -> memory_allocated_ptr;
+                    int curvirtualaddress = lowtraversal -> virtual_address;
+                    struct lownode* temp = create_lowernode(curmemlocation+sizerequired, curvirtualaddress+sizerequired,1,totsize-sizerequired);
+
+                    struct lownode* nextelement = lowtraversal->next;
+                    lowtraversal -> next = temp;
+                    temp->next = nextelement;
+                    temp->prev = lowtraversal;
+                    nextelement->prev=temp;
+                    lowtraversal -> status = 0;
+                    lowtraversal->size = sizerequired;
+                }
+
+                else{
+                    lowtraversal = lowtraversal -> next;
+                    }
+            }while(lowtraversal->next!=NULL);
+            uppertraversal=uppertraversal->next;
+        }while(uppertraversal->next!=NULL);
+        if (flag==0){
+            return -1;
+        }else{
+            return 1;
+        }
+    }else{
+        return -1;
+    }
+}
+
+
+void mems_init(){
+    header_list_space = mmap(NULL, PAGE_SIZE, PROT_READ|PROT_WRITE, MAP_PRIVATE|MAP_ANONYMOUS, -1, 0);
+    current = header_list_space;
+    current_structure_page_ptr= header_list_space;
+}
+
+
+void* mems_malloc(size_t size){
+
 }
 
 
 int main(){
-    header_list_space = mmap(NULL, PAGE_SIZE, PROT_READ|PROT_WRITE, MAP_PRIVATE|MAP_ANONYMOUS, -1, 0);
-    current = header_list_space;
-    current_structure_page_ptr= header_list_space;
+    mems_init();
 }
